3b33987e5877ecedb9350ba1ccc6f4d426001c4e
Anton Sychev <61151557+Knif-e-r@users.noreply.github.com>
2023-05-27 23:16:27 +0300

    Update main_ functions.cpp
    - made some minor edits

--- main_ functions.cpp
+++ main_ functions.cpp
@@ -1,10 +1,12 @@
 #include "functions.h"
 
+using namespace std;
+
 void see_note() {
 	ifstream list_of_notes;
 	list_of_notes.open("Notes.txt");
 	if (!list_of_notes.is_open())
-		cout << "Open file error!";
+		cout << "You haven't created your to-do list yet!\nPlease, create a few notes.\n";
 	else {
 		int note_number = 1;
 		while (!list_of_notes.eof())
@@ -45,14 +47,14 @@ void add_note() {
 	ofstream list_of_notes;
 	list_of_notes.open("Notes.txt", ofstream::app);
 	if (!list_of_notes.is_open())
-		cout << "Open file error!";
+		cout << "(Open file error! File doesn't exist!)\n";
 	else {
 		cout << "0) cancel\n\n"
 			<< "Enter new note: ";
 		string note;
 		getline(cin, note);
 
-		if (note == "0")
+		if (note.size() == 1 && note[0] == CANCEL)
 			return;
 		if (note.size() > 0) {
 			note += "\n";
@@ -75,7 +77,6 @@ void delete_note() {
 	if (!scan_note_number(note_number, notes))
 		return;
 
-	//Delete element
 	vector <string>::iterator it = notes.begin();
 	it += stoi(note_number) - 1;
 	notes.erase(it);
@@ -94,11 +95,10 @@ void change_note() {
 	vector <string> notes;
 	ifstream from_list_of_notes;
 	get_notes_arr(from_list_of_notes, notes);
-	
+
 	string note_number;
 	if (!scan_note_number(note_number, notes))
 		return;
-
 
 	//Delete element (1st step of "changing" element)
 	vector <string>::iterator it = notes.begin();
@@ -108,7 +108,7 @@ void change_note() {
 	//Insert new element (2nd step of "changing" element)
 	it = notes.begin() + (stoi(note_number) - 1);
 	cout << "Write changes: ";
-	string changed_note; 
+	string changed_note;
 	getline(cin, changed_note);
 	notes.insert(it, changed_note);
 	cout << "Note has been changed!\n";
@@ -119,7 +119,7 @@ void change_note() {
 
 
 void clear_to_do_list() {
-		ofstream list_of_notes;
-		list_of_notes.open("Notes.txt", ofstream::trunc);
-		list_of_notes.close();
+	ofstream list_of_notes;
+	list_of_notes.open("Notes.txt", ofstream::trunc);
+	list_of_notes.close();
 }
