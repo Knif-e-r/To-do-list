e93bd572ad17843b0206bd57dd40f4df7f981886
Anton Sychev <61151557+Knif-e-r@users.noreply.github.com>
2023-05-27 23:12:03 +0300

    Update additional_ functions.cpp
    - made some minor edits

--- additional_ functions.cpp
+++ additional_ functions.cpp
@@ -1,9 +1,11 @@
 #include "functions.h"
 
+using namespace std;
+
 void start_menu() {
-	cout << "\n\n_____________________________\n"
-		<< "\n\tMY TO-DO LIST\n"
-		<< "_____________________________\n"
+	cout << "\n\n_____________________________________\n"
+		<< "\n\t    MY TO-DO LIST\n"
+		<< "_____________________________________\n"
 		<< "\nWhat do you want to do?\n\n"
 		<< "1) See my notes\n"
 		<< "2) Find note\n"
@@ -15,19 +17,49 @@ void start_menu() {
 		<< "\nPlease, choose a number of action: ";
 }
 
-
+//It doesn't work correctly (has problems with big strings).
+/*
 bool sure() {
-	cout << "\n\n_____________________________\n"
-		<< "\n\tARE YOU SURE?\n"
-		<< "_____________________________\n\n"
+	system("cls");
+	cout << "\n\n_____________________________________\n"
+		<< "\n\t    ARE YOU SURE?\n"
+		<< "_____________________________________\n\n"
 		<< "0) Cancel\n"
 		<< "1) Ok\n"
 		<< "\nYour choise: ";
 
-	bool just_for_cycle = true;
+		string action; getline(cin, action);
+		if (action.size() > 1 || action.size() == 0)
+			action = "9";
 
-	while (just_for_cycle) {
+		switch (action[0]) {
+		case OK:
+			return true;
+			break;
 
+		case CANCEL:
+			return false;
+			break;
+
+		default:
+			cout << "There is no action with this number.\nPlease, try again.\n";
+			system("pause");
+			sure();
+			break;
+		};
+}
+*/
+
+bool sure() {
+	system("cls");
+	cout << "\n\n_____________________________________\n"
+		<< "\n\t    ARE YOU SURE?\n"
+		<< "_____________________________________\n\n"
+		<< "0) Cancel\n"
+		<< "1) Ok\n"
+		<< "\nYour choise: ";
+
+	while (true) {
 		string action; getline(cin, action);
 		if (action.size() > 1 || action.size() == 0)
 			action = "9";
@@ -41,9 +73,9 @@ bool sure() {
 			cout << "There is no action with this number.\nPlease, try again.\n";
 			system("pause");
 			system("cls");
-			cout << "\n\n_____________________________\n"
-				<< "\n\tARE YOU SURE?\n"
-				<< "_____________________________\n\n"
+			cout << "\n\n_____________________________________\n"
+				<< "\n\t    ARE YOU SURE?\n"
+				<< "_____________________________________\n\n"
 				<< "0) Cancel\n"
 				<< "1) Ok\n"
 				<< "\nYour choise: ";
@@ -56,6 +88,9 @@ bool scan_note_number(string& action, vector <string>& notes) {
 	cout << "\nYour answer: ";
 	getline(cin, action);
 
+	if (action.size() == 0)
+		action += "N";
+
 	bool is_symbol = false;
 	for (auto c : action) {
 		if ('0' > c || c > '9') {
@@ -64,34 +99,24 @@ bool scan_note_number(string& action, vector <string>& notes) {
 		}
 	}
 
-	if (!is_symbol) {
-		if (stoi(action) > notes.size()) {
-			while (stoi(action) > notes.size()) {
-				cout << "\nThere no note with this number.\nPlease, try again.";
-				cout << "\nYour answer: ";
-				getline(cin, action);
-				if (action[0] == CANCEL) {
-					return false;
-				}
-			}
-			return true;
-		}
+	if (!is_symbol && stoi(action) <= notes.size()) {
 		if (action[0] == CANCEL) {
 			return false;
 		}
+		return true;
 	}
 	else {
-		cout << "\nThis isn't a number.\nPlease, try again.";
+		cout << "\nThere no note with this number.\nPlease, try again.";
 		scan_note_number(action, notes);
 	}
 }
 
 
-void get_notes_arr (ifstream &from_list_of_notes, vector <string> &notes) {
-	
+void get_notes_arr(ifstream& from_list_of_notes, vector <string>& notes) {
+
 	from_list_of_notes.open("Notes.txt");
 	if (!from_list_of_notes.is_open())
-		cout << "Open file error!";
+		cout << "(Open file error! File doesn't exist!)\n";
 	else {
 		while (!from_list_of_notes.eof())
 		{
@@ -105,7 +130,7 @@ void get_notes_arr (ifstream &from_list_of_notes, vector <string> &notes) {
 }
 
 
-void send_notes_arr(ofstream &in_list_of_notes, vector <string> &notes) {
+void send_notes_arr(ofstream& in_list_of_notes, vector <string>& notes) {
 	in_list_of_notes.open("Notes.txt");
 	if (!in_list_of_notes.is_open())
 		cout << "Open file error!";
@@ -121,51 +146,45 @@ void print_info(int a) {
 
 	switch (a) {
 	case SEE:
-		cout << "\n\n_____________________________\n"
-			<< "\n     HERE ARE YOUR NOTES\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n\tHERE ARE YOUR NOTES\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	case FIND:
-		cout << "\n\n_____________________________\n"
-			<< "\n     SEARCH NEEDED NOTES\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n\tSEARCH NEEDED NOTES\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	case ADD:
-		cout << "\n\n_____________________________\n"
-			<< "\n\tADD NEW NOTE\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n\t    ADD NEW NOTE\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	case DELETE:
-		cout << "\n\n_____________________________\n"
-			<< "\n\tDELETE NOTE\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n\t    DELETE NOTE\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	case CHANGE:
-		cout << "\n\n_____________________________\n"
-			<< "\n\tCHANGE NOTE\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n\t    CHANGE NOTE\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	case CLEAR:
-		cout << "\n\n_____________________________\n"
-			<< "\n\tYOUR TO-DO LIST HAS BEEN CLEARED\n"
-			<< "_____________________________\n\n";
-		break;
-
-	case EXIT:
-		cout << "\n\n_____________________________\n"
-			<< "\n\tGOOD BYE\n"
-			<< "_____________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n  YOUR TO-DO LIST HAS BEEN CLEARED\n"
+			<< "_____________________________________\n\n";
 		break;
 
 	default:
-		cout << "\n\n_________________________________________\n"
-			<< "\n  HERE IS NO ACTIONS WITH THIS NUMBER\n"
-			<< "_________________________________________\n\n";
+		cout << "\n\n_____________________________________\n"
+			<< "\n HERE IS NO ACTIONS WITH THIS NUMBER\n"
+			<< "_____________________________________\n\n";
 		break;
 	};
 }
